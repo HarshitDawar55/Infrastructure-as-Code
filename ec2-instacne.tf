9166639e021e6b7280d5610da4e18bdc7ca7d390
HarshitDawar55 <harshit.dawar55@gmail.com>
2021-05-27 18:18:38 +0530

    Terraform Script to launch the EC2 Instance with t2.micro type & Amazon Linux 2 AMI

--- ec2-instacne.tf
+++ ec2-instacne.tf
@@ -6,3 +6,12 @@ provider "aws" {
   region  = "ap-south-1"
   profile = "Terraform"
 }
+
+resource "aws_instance" "I1" {
+  ami = "ami-010aff33ed5991201"
+  instance_type = "t2.micro"
+
+  tags = {
+    "Name" = "Resource_From_Terraform"
+  }
+}
